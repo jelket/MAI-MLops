f946ba762ff52e9f99a0a20ab0abee37d4b21a14
Stepan Aminov <31823039+jelket@users.noreply.github.com>
2025-05-03 18:27:39 +0300

    Update train.py

--- train.py
+++ train.py
@@ -15,21 +15,17 @@ def train():
         logger.error(f"Ошибка подключения: {e}")
         raise
 
-    # Загрузка данных
     iris = load_iris()
     X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2)
 
-    # Настройка MLflow
-    mlflow.set_tracking_uri("http://mlflow-server:5000")  # URL сервера MLflow
+    mlflow.set_tracking_uri("http://mlflow-server:5000")
     mlflow.set_experiment("Iris_Classification")
 
     with mlflow.start_run():
-        # Обучение модели
         model = RandomForestClassifier(n_estimators=100)
         model.fit(X_train, y_train)
         accuracy = model.score(X_test, y_test)
 
-        # Логирование параметров и метрик
         mlflow.log_param("n_estimators", 100)
         mlflow.log_metric("accuracy", accuracy)
         mlflow.sklearn.log_model(model, "iris_model")
